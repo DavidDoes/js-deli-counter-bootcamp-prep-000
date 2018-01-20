6a36e061eb9a771125c427699ab87f5c2878ef40
David Clark <dlc.clark@gmail.com>
2018-01-20 23:03:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,14 +14,23 @@ function nowServing(line){
   }
 }
 
-function currentLine(line){
-  if (line.length === 0){
-    return "The line is currently empty."
-  } else {
-    var namesAndNumbers = [];
-    for (var i = 0; i<line.length; i++) {
-    namesAndNumbers.push(i+1 + ". "+ line[i]);
+function currentLine(arr) {
+    var line = []
+    if (arr.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < arr.length; i++) {
+          //remember: don't have to define line as var
+        line += (i + 1) + ". " + arr[i] + ", "
+          // (i + 1) = start from 1 instead of 0
+          // + ". " = add a "."
+          // arr[i] + ", " = add ", " at current iteration of arr
+          //"1. Bob, "
+      }
+      line = line.slice(0, line.length-2)
+        //removing the last comma
+        //we add commas with `line +=` above, so we must
+        // remove the last one.
+      return "The line is currently: " + line
     }
-  return `The line is currently: ${namesAndNumbers.join(', ')}`
-  }
 }
